ed41d5cf7814452d3624489b331703926eb62de3
Karin2013 <90284610+Karin2013@users.noreply.github.com>
2022-03-31 18:09:14 -0400

    Add files via upload

---
+++ collection/js/main.js
@@ -0,0 +1,32 @@
+var touch = document.querySelector(".touch");
+var menu = document.querySelector(".menu");
+
+touch.addEventListener('click', function() {
+    touch.classList.toggle('touch-active'),
+    menu.classList.toggle('menu-active'),
+    
+  
+    if (menu.getAttribute("menu") == menu.style) {
+      touch.style = touch.getAttribute("touch-active"),
+      menu.style = menu.getAttribute("menu-active")
+    } else {
+      btn1.getAttribute("text1-active", btn1.style);
+      btn1.style = btn1.getAttribute("text1"),
+      myself.style = myself.getAttribute("myself")
+      myself1.style = myself1.getAttribute("myself1")
+      myself2.style = myself2.getAttribute("myself2")
+      myself3.style = myself3.getAttribute("myself3")
+      myself4.style = myself4.getAttribute("myself4")
+      myself5.style = myself5.getAttribute("myself5")
+      myself6.style = myself6.getAttribute("myself6")
+      myself7.style = myself7.getAttribute("myself7")
+      myself8.style = myself8.getAttribute("myself8")
+      myself9.style = myself9.getAttribute("myself9")
+      myself10.style = myself10.getAttribute("myself10")
+      myself11.style = myself11.getAttribute("myself11")
+      myself12.style = myself12.getAttribute("myself12")
+      myself13.style = myself13.getAttribute("myself13");
+    }
+  }, false);
+
+  
